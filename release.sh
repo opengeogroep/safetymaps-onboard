2029e067c98d880b4bdff793fecd8c93da7e91f8
Matthijs Laan <matthijsln@gmail.com>
2019-10-23 13:11:00 +0200

    area srid param toegevoegd

--- release.sh
+++ release.sh
@@ -10,6 +10,8 @@ cp safetymaps-onboard.cmd bin
 source ${1:-./bag_settings.sh}
 echo Querying bag database \(host=$PGHOST,port=$PGPORT,user=$PGUSER,db=$PGDATABASE\)
 echo Organisation area: $AREA
+AREA_SRID=${AREA_SRID:-4326}
+echo Organisation area SRID: $AREA_SRID
 psql -h $PGHOST -p $PGPORT -U $PGUSER $PGDATABASE -c "select openbareruimtenaam || ' ' \
   || COALESCE(CAST(huisnummer as varchar) || ' ','') \
   || COALESCE(CAST(huisletter as varchar) || ' ','') \
@@ -21,7 +23,7 @@ psql -h $PGHOST -p $PGPORT -U $PGUSER $PGDATABASE -c "select openbareruimtenaam 
   END as display_name, \
   st_x(st_transform(geopunt,4326)) as x, st_y(st_transform(geopunt,4326)) as y \
   from bag_actueel.adres \
-  where st_contains(st_transform(st_setsrid(st_geomfromtext('$AREA'),4326),28992),geopunt)" -Aztq > bag.txt
+  where st_contains(st_transform(st_setsrid(st_geomfromtext('$AREA'),$AREA_SRID),28992),geopunt)" -Aztq > bag.txt
 echo Creating Lucene index...
 rm -rf db 2>/dev/null
 java -cp bin/safetymaps-onboard-*.jar nl.opengeogroep.safetymaps.onboard.IndexBuilder bag.txt
